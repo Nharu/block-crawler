8deb8ee4f0890e631ff6c47b76f1990ce314781a
Unknown <ljh1997213@gmail.com>
2019-02-12 16:14:21 +0900

    [Add] ethereum txs

--- block.py
+++ block.py
@@ -3,28 +3,31 @@ import sys
 import csv
 
 # Select which coin to archive
+print()
 print('select coin')
 print('1: ethereum')
-s0 = int(input('select: '))
+s = int(input('select: '))
 
-if s0 == 1:
+if s == 1:
+	print()
 	print('select category')
 	print('1: blocks')
-	s1 = int(input('select: '))
+	print('2: transactions')
+	s = int(input('select: '))
 
-	if s1 == 1:
+	if s == 1:
 		r = requests.get('https://api.blockchair.com/ethereum/blocks').json()
-		id = r['data'][0]['id']
+		lid = r['data'][0]['id']
 
 		start = int(input('start block: '))
-		end = int(input('end block(current last block#{0}): '.format(id)))
+		end = int(input('end block(current last block#{0}): '.format(lid)))
 
 		# error handling
 		if start < 0:
 			print('error')
 			sys.exit(1)
 
-		elif end > id:
+		elif end > lid:
 			print('error')
 			sys.exit(1)
 
@@ -47,6 +50,82 @@ if s0 == 1:
 
 				w.writerow(bd.values())
 
+	elif s == 2:
+		print()
+		print('select option')
+		print('1: set range with tx#')
+		print('2: set range with block#')
+		s = int(input('select: '))
 
+		if s == 1:
+			r = requests.get('https://api.blockchair.com/ethereum/transactions').json()
+			lid = r['data'][0]['id']
 
+			start = int(input('start tx: '))
+			end = int(input('end tx(current last tx#{0}): '.format(lid)))
 
+			# error handling
+			if start < 0:
+				print('error')
+				sys.exit(1)
+
+			elif end > lid:
+				print('error')
+				sys.exit(1)
+
+			elif start > end:
+				print('error')
+				sys.exit(1)
+
+			# open csv
+			name = input('set csv file name: ')
+			with open('{0}.csv'.format(name), 'w') as f:
+				w = csv.writer(f)
+
+				for i in range(start, end + 1):
+					print('collecting tx#{0}'.format(i))
+					b = requests.get('https://api.blockchair.com/ethereum/transactions?q=id({0})'.format(i)).json()
+					bd = b['data'][0]
+
+					if i == start:
+						w.writerow(bd.keys())
+
+					w.writerow(bd.values())
+
+		elif s == 2:
+			r = requests.get('https://api.blockchair.com/ethereum/blocks').json()
+			lid = r['data'][0]['id']
+
+			start = int(input('start block: '))
+			end = int(input('end block(current last block#{0}): '.format(lid)))
+
+			# error handling
+			if start < 0:
+				print('error')
+				sys.exit(1)
+
+			elif end > lid:
+				print('error')
+				sys.exit(1)
+
+			elif start > end:
+				print('error')
+				sys.exit(1)
+
+			# open csv
+			name = input('set csv file name: ')
+			with open('{0}.csv'.format(name), 'w') as f:
+				w = csv.writer(f)
+
+				for i in range(start, end + 1):
+					print('collecting block#{0}'.format(i))
+					b = requests.get('https://api.blockchair.com/ethereum/transactions?q=block_id({0})'.format(i)).json()
+					bd = b['data']
+
+					if i == start:
+						w.writerow(bd[0].keys())
+
+					for tx in bd:
+						w.writerow(tx.values())
+
+						
